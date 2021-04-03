5492110d8466fd05fa87a9d4067ec547255faf5b
Olya <zaychenok37@gmail.com>
2021-04-03 18:14:06 +0300

    Add router tasks

--- router.js
+++ router.js
@@ -5,12 +5,16 @@ const { checkUser } = require('./middlewares/user.mw');
 const router = Router();
 
 router.get('/users', UserController.getAllUsers);
+router.get('/users', UserController.getOneUser);
 router.post('/user', UserController.createUser);
 router.patch('/user/:id', UserController.updateUser);
 router.patch('/user-v2/:id', checkUser, UserController.updateUserInstance);
 router.delete('/user/:id', UserController.deleteUser);
 
+router.get('/tasks', TaskController.getAllTasks);
 router.get('/user/:id/task', checkUser, TaskController.getUserTasks);
-router.post('/user/:id/task', checkUser, TaskController.createTask);
+router.post('/task', TaskController.createTask);
+router.patch('/task/:id', TaskController.updateTask);
+router.delete('/task/:id', TaskController.deleteTask);
 
 module.exports = router;
